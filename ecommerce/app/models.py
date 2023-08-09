879051dde34ec0b48cf39ddbac85ae7b1ad81e5c
CongSanhLe <congsanhle1999@gmail.com>
2023-08-10 03:36:31 +0700

    [1008-22] Category models

--- ecommerce/app/models.py
+++ ecommerce/app/models.py
@@ -5,8 +5,20 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your models here.
 
 
+class Category(models.Model):
+    sub_category = models.ForeignKey(
+        "self", on_delete=models.CASCADE, related_name="sub_categories", null=True, blank=True)
+    is_sub = models.BooleanField(default=False)
+    name = models.CharField(max_length=200, null=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
+    category = models.ManyToManyField(Category, related_name="product")
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
